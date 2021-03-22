01b1d36caaf46552ed20f7cd59920e8ed5b49801
EdChekanov <kide.da@gmail.com>
2021-03-22 22:38:42 +0300

    complete 2 tasks

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -3,7 +3,12 @@ const CustomError = require("../extensions/custom-error");
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function dateSample(SAMPLE_ACTIVITY) {
+  console.log(typeof(SAMPLE_ACTIVITY));
+    if (SAMPLE_ACTIVITY > 15 || SAMPLE_ACTIVITY <= 0 || typeof(SAMPLE_ACTIVITY) !== 'string'  || isNaN(+SAMPLE_ACTIVITY)) {
+      return false;
+    };
+    SAMPLE_ACTIVITY = parseFloat(SAMPLE_ACTIVITY);
+    let time = Math.log(MODERN_ACTIVITY/SAMPLE_ACTIVITY)/(0,693/HALF_LIFE_PERIOD);
+    return Math.ceil(time * 1000);
+  };
--- src/count-cats.js
+++ src/count-cats.js
@@ -1,6 +1,17 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function countCats(/* matrix */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function countCats(x) {
+  let count = 0;
+  let i = x.length - 1;
+  while (i >= 0) {
+    let n = x[i].length - 1;
+    while (n >= 0) {
+      if (x[i][n] == '^^') {
+        count += 1;
+      }
+      n--;
+    }
+    i--;
+  }
+  return count;
 };
