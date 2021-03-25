106172534ddccf1bef2658304438440f4429cca3
Ed Chekanov <kide.da@gmail.com>
2021-03-25 15:12:20 +0300

    mind deal boom

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,25 +1,67 @@
 const CustomError = require("../extensions/custom-error");
 
 const chainMaker = {
+  chain: '',
+  // countItems: this.chain.split('~~'),
+  
+
   getLength() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    return this.chain.length;
   },
   addLink(value) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    if (value === undefined) {
+      value = ' ';
+    };
+    this.chain = this.chain + `( ${value} )~~`;
+    return this;
   },
   removeLink(position) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    if (position > this.chain.slice(0, -2).split('~~').length || position < 1 || position ===undefined) {
+      throw new Error;
+    }
+    this.chain = this.chain.split('~~');
+    for (let i = 0; i < this.chain.length; i++) {
+      if (i === position - 1) {
+        if (i + 1 === this.chain.length) {
+          this.chain.shift(this.chain[0]);
+        } else {
+      this.chain.shift(this.chain[0]);
+      this.chain.push(this.chain[0]);
+      this.chain.shift(this.chain[0]);
+      }
+      } else {
+      this.chain.push(this.chain[0]);
+      this.chain.shift(this.chain[0]);
+    }
+    }
+    
+    this.chain = this.chain.join('~~');
+    return this;
   },
   reverseChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    if (this.chain == '') {} else {
+    this.chain = this.chain.slice(0, -2).split('~~');
+    let length = this.chain.length;
+    for (let i = length - 1; i >= 0; i--) {
+      this.chain.push(this.chain[i]);
+    }
+    for (let i = length - 1; i >= 0; i--) {
+      this.chain.shift(this.chain[0]);
+    }
+    if (this.chain.length == 1) {
+    this.chain = String(this.chain) + '~~';  
+    } else {
+    this.chain = this.chain.join('~~') + '~~';
+    };
+    }
+    return this;
   },
   finishChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    if (this.chain[this.chain.length - 1] == '~') {
+    return this.chain.slice(0, -2);
+    } else {
+      return this.chain;
+    }
   }
 };
 
